500df50c26312a0d0d7bdaa0a88dbb946f0c5fdd
Arthur Aukhatov <auhatov@cloudally.com>
2020-09-02 17:06:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Java Benchmarks
+
+## String Concatenation Benchmark
+
+### Results of the Benchmark
+```
+Benchmark                               (randomWordLength)  Mode  Cnt    Score    Error  Units
+StringBenchmark.primitiveConcatenation                  10  avgt   10   24.203 ±  0.211  ns/op
+StringBenchmark.primitiveConcatenation                  20  avgt   10   25.040 ±  0.364  ns/op
+StringBenchmark.primitiveConcatenation                  50  avgt   10   38.281 ±  0.895  ns/op
+StringBenchmark.stringBuilder                           10  avgt   10   20.765 ±  0.271  ns/op
+StringBenchmark.stringBuilder                           20  avgt   10   22.045 ±  0.611  ns/op
+StringBenchmark.stringBuilder                           50  avgt   10   38.298 ±  1.023  ns/op
+StringBenchmark.stringConcat                            10  avgt   10   64.063 ±  0.343  ns/op
+StringBenchmark.stringConcat                            20  avgt   10   67.399 ±  0.763  ns/op
+StringBenchmark.stringConcat                            50  avgt   10   76.619 ±  2.591  ns/op
+StringBenchmark.stringFormat                            10  avgt   10  673.458 ± 26.151  ns/op
+StringBenchmark.stringFormat                            20  avgt   10  711.718 ± 50.907  ns/op
+StringBenchmark.stringFormat                            50  avgt   10  733.963 ± 29.483  ns/op
+```
+
+### Conclusion
+
+Dear Java Developers, please avoid using `String.format()` to concatenate a string.
---
+++ src/main/java/art/aukhatov/string/StringBenchmark.java
@@ -0,0 +1,75 @@
+package art.aukhatov.string;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@Fork(value = 1, jvmArgsAppend = {"-Xms128m", "-Xmx128m"})
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 5, time = 1)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class StringBenchmark {
+
+    @Benchmark
+    public String primitiveConcatenation(Dictionary dictionary, Blackhole bh) {
+        String message = "Hello " + dictionary.word + dictionary.word;
+        bh.consume(message);
+        return message;
+    }
+
+    @Benchmark
+    public String stringConcat(Dictionary dictionary, Blackhole bh) {
+        String message = "Hello ".concat(dictionary.word).concat(dictionary.word);
+        bh.consume(message);
+        return message;
+    }
+
+    @Benchmark
+    public String stringFormat(Dictionary dictionary, Blackhole bh) {
+        String pattern = "Hello %s %s";
+        String message = String.format(pattern, dictionary.word, dictionary.word);
+        bh.consume(message);
+        return message;
+    }
+
+    @Benchmark
+    public String stringBuilder(Dictionary dictionary, Blackhole bh) {
+        StringBuilder builder = new StringBuilder();
+        String message = builder
+                .append("Hello")
+                .append(dictionary.word)
+                .append(dictionary.word)
+                .toString();
+        bh.consume(message);
+        return message;
+    }
+
+    @State(Scope.Thread)
+    public static class Dictionary {
+
+        String word;
+
+        @Param({"10", "20", "50"})
+        int randomWordLength;
+
+        @Setup
+        public void setUp() {
+            String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            word = RandomStringUtils.random(randomWordLength, characters);
+        }
+    }
+}
